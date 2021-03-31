96d326cd4ee1569cf9f2ab250913f2ac0bc82761
Isamu Ito <isamsito@gmail.com>
2021-03-31 01:20:56 +0000

    complete exercise 4 and 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,46 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+
+store = Store.new do |s|
+  s.name = "Surrey"
+  s.annual_revenue = 224000
+  s.mens_apparel = false
+  s.womens_apparel = true
+  s.save
+end
+
+store = Store.new do |s|
+  s.name = "Whistler"
+  s.annual_revenue = 1900000
+  s.mens_apparel = true
+  s.womens_apparel = false
+  s.save
+end
+
+store = Store.new do |s|
+  s.name = "Yaletown"
+  s.annual_revenue = 430000
+  s.mens_apparel = true
+  s.womens_apparel = true
+  s.save
+end
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where(womens_apparel: true)
+
+puts "--- Mens Stores ---"
+@mens_stores.each do |store|
+  puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+puts "--- Womens Stores ---"
+@womens_stores.map do |store|
+  if store.annual_revenue < 1000000
+    puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+  end
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
+@successful_stores = Store.where("annual_revenue > 1000000").count(:id)
+puts "total revenue #{@total_revenue}"
+puts "Average revenue #{@average_revenue}"
+p @successful_stores
